8f48bda9bd50353a6d6ec302c4a4574d078cae51
Pavlo Sprogis <pavel.sprogis@gmail.com>
2021-01-31 23:02:34 +0200

    add sorting

--- query.sql
+++ query.sql
@@ -1,7 +1,9 @@
 -- Show all countries with more than 1000 (4) users, sorted by user count. The
 -- country with the most users should be at the top.
 
-SELECT c.id, name, iso, users.citizenship_country_id FROM countries as c
+SELECT c.id, name, iso, users.citizenship_country_id, count(citizenship_country_id) as citizens FROM countries c
 INNER JOIN users ON users.citizenship_country_id = c.id
 GROUP BY c.id
-HAVING count(citizenship_country_id) > 4
+-- HAVING count(citizenship_country_id) > 4
+HAVING citizens > 4
+ORDER BY citizens DESC
